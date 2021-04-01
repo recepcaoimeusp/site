cb11e3ea38966ff1358a97e798e4c2e5d9288a35
betitobeto <robertobolgheroni@usp.br>
2021-04-01 18:24:58 -0300

    Adicionando carrossel padrao para imagens

---
+++ components/AppCarrossel.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="carrossel-conteiner" :style="estiloCarrossel">
+    <v-carousel hide-delimiters :height="altura">
+      <v-carousel-item v-for="(link, i) in links" :key="i">
+        <v-card
+          class="conteiner-item d-flex justify-center align-center"
+          :style="{ backgroundImage: `url(${link})` }"
+        />
+      </v-carousel-item>
+    </v-carousel>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    links: {
+      type: Array,
+      required: true,
+    },
+    altura: {
+      type: [String, Number],
+      default: 300,
+    },
+    largura: {
+      type: [String, Number],
+      default: 500,
+    },
+  },
+  computed: {
+    estiloCarrossel() {
+      return {
+        maxWidth: `${this.largura}px`,
+      }
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.carrossel-conteiner {
+  width: 100%;
+  .conteiner-item {
+    height: 100%;
+    background-position: center;
+    background-size: cover;
+  }
+}
+</style>
