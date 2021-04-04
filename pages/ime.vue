eff135c52709b2ec3acb77569da6ff84889321d3
José Lucas Silva Mayer <josemayer@usp.br>
2021-04-04 03:27:56 -0400

    Corrigir os erros do código e padronizá-lo com o lint
    Signed-off-by: José Lucas Silva Mayer <josemayer@usp.br>

--- pages/ime.vue
+++ pages/ime.vue
@@ -7,25 +7,46 @@
             max-width="600"
             class="d-flex flex-column flex-sm-row justify-center align-center pa-5"
           >
-            <img width="150px" class="mr-sm-5 mb-5 mb-sm-0" src="~/assets/images/logoaaamat.png"/>
-            <p>
-              A Associação Atlética Acadêmica da Matemática é uma instituição gerenciada por um grupo de alunos
-              do IME que cuidam da organização de todas as modalidades esportivas, campeonatos e inters. Além disso,
-              realizam festas e eventos de integração e também vendem produtos personalizados do instituto. Se você
-              tem interesse em participar de alguma modalidade ou tem alguma dúvida é só entrar em contato pelas redes sociais.
-            <table class="socialMedia">
-              <tr>
-                <td class="nome text-center text-md-left">
-                  <img class="icon" width="50px" src="~/assets/images/facebook.svg"/>
-                  <a href="https://www.facebook.com/aaamat.ime" class="link">aaamat.ime</a>
-                </td>
-                <td class="nome text-center text-md-left">
-                  <img class="icon" width="50px" src="~/assets/images/instagram.svg"/>
-                  <a href="https://www.instagram.com/aaamat_imeusp/" class="link">aaamat_imeusp</a>
-                </td>
-              </tr>
-            </table>
-            </p>
+            <img
+              width="150px"
+              class="mr-sm-5 mb-5 mb-sm-0"
+              src="~/assets/images/logoaaamat.png"
+            />
+            <span>
+              A Associação Atlética Acadêmica da Matemática é uma instituição
+              gerenciada por um grupo de alunos do IME que cuidam da organização
+              de todas as modalidades esportivas, campeonatos e inters. Além
+              disso, realizam festas e eventos de integração e também vendem
+              produtos personalizados do instituto. Se você tem interesse em
+              participar de alguma modalidade ou tem alguma dúvida é só entrar
+              em contato pelas redes sociais.
+              <table class="socialMedia">
+                <tr>
+                  <td class="nome text-center text-md-left">
+                    <img
+                      class="icon"
+                      width="50px"
+                      src="~/assets/images/facebook.svg"
+                    />
+                    <a href="https://www.facebook.com/aaamat.ime" class="link"
+                      >aaamat.ime</a
+                    >
+                  </td>
+                  <td class="nome text-center text-md-left">
+                    <img
+                      class="icon"
+                      width="50px"
+                      src="~/assets/images/instagram.svg"
+                    />
+                    <a
+                      href="https://www.instagram.com/aaamat_imeusp/"
+                      class="link"
+                      >aaamat_imeusp</a
+                    >
+                  </td>
+                </tr>
+              </table>
+            </span>
           </v-card>
         </v-col>
         <v-col
@@ -42,7 +63,7 @@
           md="6"
           class="d-flex text-center text-md-right justify-md-center justify-center align-center"
         >
-        <AppCarrossel :largura="500" :altura="400" :links="fotosAtletica" />
+          <AppCarrossel :largura="500" :altura="400" :links="fotosAtletica" />
         </v-col>
         <v-col
           cols="12"
@@ -51,15 +72,21 @@
         >
           <div class="imeDescContainer">
             <p>
-              O Instituto de Matemática e Estatística é uma unidade de ensino, pesquisa e extensão da Universidade de São Paulo. 
-              Possui cursos de graduação e pós-graduação nas áreas de matemática, matemática aplicada, estatística e ciência da 
-              computação. Além disso, com corpo docente e discente qualificado, realiza pesquisas científicas em suas áreas-chave 
-              e áreas relacionadas, como bioinformática, neuromatemática e educação em matemática. Tem como atividades de extensão, 
-              abertas à comunidade externa: cursos de aperfeiçoamento e oficinas para professores da rede pública, cursos de 
-              matemática e de computação durante o período de recesso do calendário letivo de graduação, assessoria de estatística 
-              e de software livre para pesquisadores, empresas e órgãos públicos. Também promove exposições de objetos matemáticos 
-              com o objetivo de propagar conceitos matemáticos diversos à comunidade em geral, além de motivar e ilustrar conteúdos 
-              ministrados em sala de aula.
+              O Instituto de Matemática e Estatística é uma unidade de ensino,
+              pesquisa e extensão da Universidade de São Paulo. Possui cursos de
+              graduação e pós-graduação nas áreas de matemática, matemática
+              aplicada, estatística e ciência da computação. Além disso, com
+              corpo docente e discente qualificado, realiza pesquisas
+              científicas em suas áreas-chave e áreas relacionadas, como
+              bioinformática, neuromatemática e educação em matemática. Tem como
+              atividades de extensão, abertas à comunidade externa: cursos de
+              aperfeiçoamento e oficinas para professores da rede pública,
+              cursos de matemática e de computação durante o período de recesso
+              do calendário letivo de graduação, assessoria de estatística e de
+              software livre para pesquisadores, empresas e órgãos públicos.
+              Também promove exposições de objetos matemáticos com o objetivo de
+              propagar conceitos matemáticos diversos à comunidade em geral,
+              além de motivar e ilustrar conteúdos ministrados em sala de aula.
             </p>
             <p class="text-md-left text-center">
               <v-btn
@@ -81,21 +108,24 @@
         >
           <div class="mapinhaDescContainer">
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod elit quis mollis blandit. 
-              Nam eleifend tortor vel est tincidunt, in accumsan orci porttitor. Aenean sodales nibh lectus, in 
-              fringilla purus suscipit eu. Nam quis augue gravida, lobortis eros sed, lacinia leo. Proin viverra 
-              tortor et odio commodo, sed sagittis velit mollis. Morbi ornare leo id eros maximus, eget aliquam mi 
-              dignissim. In eu ultricies est. Vivamus ut finibus neque. Suspendisse massa mauris, tempus congue lobortis 
-              nec, finibus eu sem.
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+              euismod elit quis mollis blandit. Nam eleifend tortor vel est
+              tincidunt, in accumsan orci porttitor. Aenean sodales nibh lectus,
+              in fringilla purus suscipit eu. Nam quis augue gravida, lobortis
+              eros sed, lacinia leo. Proin viverra tortor et odio commodo, sed
+              sagittis velit mollis. Morbi ornare leo id eros maximus, eget
+              aliquam mi dignissim. In eu ultricies est. Vivamus ut finibus
+              neque. Suspendisse massa mauris, tempus congue lobortis nec,
+              finibus eu sem.
             </p>
-          <p class="text-md-right text-center">
-            <v-btn
-              color="#F22816"
-              class="white--text mt-5"
-              href="http://ime.usp.br"
-            >Conheça o mapinha!
-            </v-btn>
-          </p>
+            <p class="text-md-right text-center">
+              <v-btn
+                color="#F22816"
+                class="white--text mt-5"
+                href="http://ime.usp.br"
+                >Conheça o mapinha!
+              </v-btn>
+            </p>
           </div>
         </v-col>
         <v-col
@@ -112,9 +142,7 @@
 
 <script>
 export default {
-  components: {
-
-  },
+  components: {},
   data: () => ({
     ids: [
       '10B5cfEwqkjmzZu-6sT98KKy3bZRmjSyU',
