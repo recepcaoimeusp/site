ecdb1e4525ff6cd95c9bbe107ac31225c2dbc171
José Lucas Silva Mayer <josemayer@usp.br>
2021-04-05 05:36:33 -0400

    Adicionar dois componentes para abstrair as barras comuns da página do IME.
    Signed-off-by: José Lucas Silva Mayer <josemayer@usp.br>

---
+++ components/SecondaryBar.vue
@@ -0,0 +1,72 @@
+<template>
+  <v-row :class="{ 'background-azul': backgroundAzul }">
+    <v-col
+      cols="12"
+      md="6"
+      class="d-flex text-center text-md-right justify-md-center justify-center align-center"
+    >
+      <div v-if="alinhaTextoEsquerda" :style="defineLarguraContainer">
+        <slot></slot>
+      </div>
+      <AppCarrossel
+        v-if="!alinhaTextoEsquerda"
+        :largura="larguraSlide"
+        :altura="alturaSlide"
+        :links="links"
+      />
+    </v-col>
+    <v-col
+      cols="12"
+      md="6"
+      class="d-flex justify-center justify-md-center align-center"
+    >
+      <div v-if="!alinhaTextoEsquerda" :style="defineLarguraContainer">
+        <slot></slot>
+      </div>
+      <AppCarrossel
+        v-if="alinhaTextoEsquerda"
+        :largura="larguraSlide"
+        :altura="alturaSlide"
+        :links="links"
+      />
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  props: {
+    backgroundAzul: {
+      type: Boolean,
+      default: false,
+    },
+    links: {
+      type: Array,
+      required: true,
+    },
+    alturaSlide: {
+      type: [String, Number],
+      default: 300,
+    },
+    larguraSlide: {
+      type: [String, Number],
+      default: 500,
+    },
+    alinhaTextoEsquerda: {
+      type: Boolean,
+      required: true,
+    },
+    larguraContainer: {
+      type: Number,
+      default: 600,
+    },
+  },
+  computed: {
+    defineLarguraContainer() {
+      return {
+        maxWidth: `${this.larguraContainer}px`,
+      }
+    },
+  },
+}
+</script>
