dab50d8299f61ff9d87b7eaf328d992301bd7360
José Lucas Silva Mayer <josemayer@usp.br>
2021-04-06 04:19:29 -0400

    Criar o novo componente PageBar, a fim de abstrair a estrutura comum de barra da página
    Signed-off-by: José Lucas Silva Mayer <josemayer@usp.br>

---
+++ components/PageBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-row justify="end" class="mb-0 py-8" :class="{ 'background-azul': backgroundAzul }">
+    <v-col 
+      cols="12" 
+      md="6" 
+      class="d-flex justify-center align-center text-center text-md-right"
+    >
+      <slot name="left"></slot>
+    </v-col>
+    <v-col
+      cols="12"
+      md="6"
+      class="d-flex justify-center align-center"
+    >
+      <slot name="right"></slot>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  props: {
+    backgroundAzul: {
+      type: Boolean,
+      default: false,
+    },
+  },
+}
+</script>
