4d663d196b195b02d3e2e81e412b523c265ae01f
betitobeto <robertobolgheroni@usp.br>
2021-04-08 16:10:40 -0300

    Tabela de eventos na pagina da semana

--- pages/semana.vue
+++ pages/semana.vue
@@ -1,26 +1,35 @@
 <template>
-  <v-row justify="space-between" align="center">
-    <v-col cols="12" lg="5" xl="4">
-      <h2>Semana de Recepção</h2>
-      <p>
-        Texto explicando o tema, o que é a semana, como funciona, pedindo pra
-        fazer o cadastro
-        <br />
-        <br />
-        <span>A semana será dia tal até tal</span>
-      </p>
+  <v-row justify="space-between" align="start">
+    <v-col class="fundo-azul" cols="12">
+      <v-container fluid>
+        <v-row justify="space-between" align="start">
+          <v-col cols="12" lg="5" xl="4">
+            <h2>Semana de Recepção</h2>
+            <p>
+              Texto explicando o tema, o que é a semana, como funciona, pedindo
+              pra fazer o cadastro
+              <br />
+              <br />
+              <span>A semana será dia tal até tal</span>
+            </p>
 
-      <div class="d-flex container row justify-start links">
-        <a class="link" href="#">
-          <v-btn depressed tile color="#FECA09"> Link dos links </v-btn>
-        </a>
-        <a class="link" href="#">
-          <v-btn depressed tile color="#FECA09"> Acessse o Guia do Bixe </v-btn>
-        </a>
-      </div>
-    </v-col>
-    <v-col cols="12" lg="7" xl="8">
-      <EventosTabela />
+            <div class="d-flex container row justify-start links">
+              <a class="link" href="#">
+                <v-btn depressed tile color="#FECA09"> Link dos links </v-btn>
+              </a>
+              <a class="link" href="#">
+                <v-btn depressed tile color="#FECA09">
+                  Acessse o Guia do Bixe
+                </v-btn>
+              </a>
+            </div>
+          </v-col>
+          <v-col cols="12" lg="7" xl="8">
+            <h2 class="mb-5">Cronograma</h2>
+            <EventosTabela />
+          </v-col>
+        </v-row>
+      </v-container>
     </v-col>
     <v-col cols="12">
       <h2 class="mb-3">Provas da Gincana</h2>
@@ -33,7 +42,6 @@
           tag="li"
           outlined
           class="prova d-flex flex-column mr-0 mr-sm-5 mb-5"
-          width="280"
           height="180"
         >
           <v-card-title>
@@ -50,6 +58,11 @@
           </div>
         </v-card>
       </ul>
+    </v-col>
+    <v-col cols="12 d-flex justify-center">
+      <v-btn x-large color="error" @click="ganharPontos"
+        >Perder 10 pontos</v-btn
+      >
     </v-col>
   </v-row>
 </template>
@@ -99,10 +112,21 @@ export default {
       },
     ],
   }),
+  methods: {
+    ganharPontos() {
+      alert(
+        'Você perdeu 10 pontos. Mas também ganhou 10! Basta confiar na palavra fricasse.'
+      )
+    },
+  },
 }
 </script>
 
 <style lang="scss" scoped>
+.fundo-azul {
+  background: #a2b7e2;
+}
+
 .links {
   .link {
     &:not(:last-child) {
@@ -114,6 +138,11 @@ export default {
 
 .provas-grid {
   .prova {
+    width: 100%;
+    max-width: 400px;
+    @media (min-width: 600px) {
+      max-width: 300px;
+    }
     .cadeado-conteiner {
       width: 100%;
       height: 100%;
