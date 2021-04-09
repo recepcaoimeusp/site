705fd0854323da92941628771018a73ac5967262
Thiago Guerrero <tgbalera@hotmail.com>
2021-04-09 14:21:30 -0300

    Refactoring
    Signed-off-by: Thiago Guerrero <tgbalera@hotmail.com>
    Signed-off-by: Luiz Carlos <luizcarlosdk@hotmail.com>

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -2,7 +2,7 @@
   <div>
     <v-app-bar fixed app color="#0B122F" class="white--text hidden-md-and-down">
       <v-toolbar-title>
-        <NuxtLink class="white--text nav-link" to="/">
+        <NuxtLink class="white--text" to="/">
           Comissão de Recepção IME USP
         </NuxtLink>
       </v-toolbar-title>
@@ -10,14 +10,15 @@
       <v-btn
         color="#FECA09"
         class="black--text font-weight-bold"
-        @click="mensagemNaoExiste"
+        :href="forms"
+        target="_blank"
         >Bixe, cadastre-se aqui</v-btn
       >
       <template v-for="(link, i) in links">
         <NuxtLink
           v-if="!link.ehExterno"
           :key="`link-${i}`"
-          class="mx-5 white--text nav-link"
+          class="mx-5 white--text"
           :to="link.url"
           >{{ link.nome }}</NuxtLink
         >
@@ -25,7 +26,7 @@
           v-else
           :key="`link-${i}`"
           :href="link.url"
-          class="ml-5 white--text nav-link"
+          class="ml-5 white--text"
           target="_blank"
           >{{ link.nome }}</a
         >
@@ -42,21 +43,26 @@
           <v-btn
             color="#FECA09"
             class="black--text font-weight-bold my-5"
-            @click="mensagemNaoExiste"
+            :href="forms"
+            target="_blank"
             >Bixe, cadastre-se aqui</v-btn
           >
           <v-list-item to="/">
             <v-list-item-title>Página Inicial</v-list-item-title>
           </v-list-item>
-          <v-list-item
-            v-for="(link, i) in links"
-            :key="i"
-            :to="link.ehExterno ? undefined : link.url"
-            :href="link.ehExterno ? link.url : undefined"
-            :target="link.ehExterno ? '_blank' : ''"
-          >
-            <v-list-item-title>{{ link.nome }}</v-list-item-title>
-          </v-list-item>
+          <template v-for="(link, i) in links">
+            <v-list-item
+              v-if="link.ehExterno"
+              :key="i"
+              :href="link.url"
+              target="_blank"
+            >
+              <v-list-item-title>{{ link.nome }}</v-list-item-title>
+            </v-list-item>
+            <v-list-item v-else :key="i" :to="link.url">
+              <v-list-item-title>{{ link.nome }}</v-list-item-title>
+            </v-list-item>
+          </template>
         </v-list-item-group>
       </v-list>
     </v-navigation-drawer>
@@ -69,6 +75,7 @@ export default {
   data: () => ({
     drawer: false,
     mostrarInput: false,
+    forms: 'https://forms.gle/UuvHzWwkHauGw2z27',
     links: [
       { url: '/comissao', nome: 'Quem somos?', ehExterno: false },
       { url: '/semana', nome: 'Semana de Recepção', ehExterno: false },
@@ -94,18 +101,5 @@ export default {
       },
     ],
   }),
-  methods: {
-    mensagemNaoExiste() {
-      alert(
-        'Este recurso ainda não está disponível. Por favor, aguarde nossas atualizações.'
-      )
-    },
-  },
 }
 </script>
-
-<style lang="scss">
-.nav-link {
-  text-decoration: none;
-}
-</style>
