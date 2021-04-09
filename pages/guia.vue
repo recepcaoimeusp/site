4214836809376e6d3bb47c9d6c54e37f2804479b
Thiago Guerrero <tgbalera@hotmail.com>
2021-04-09 18:59:22 -0300

    refactoring guia do bixe page
    Signed-off-by: Thiago Guerrero <tgbalera@hotmail.com>

--- pages/guia.vue
+++ pages/guia.vue
@@ -1,28 +1,22 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" class="d-flex justify-center">
+  <v-row>
+    <v-col cols="12" align="center" class="mt-16">
       <v-card max-width="344">
         <v-img
           :src="require('@/assets/images/construcao.gif')"
           height="200px"
         ></v-img>
 
-        <v-card-text>
+        <v-card-text class="text--primary">
           Desculpe, mas o Guia do Bixe ainda está em construção :/ Por favor,
           tente novamente mais tarde!
         </v-card-text>
         <v-card-actions>
           <v-btn color="orange lighten-2" block @click="$router.go(-1)">
-            <a @click.prevent=""> Ok, Voltar :/ </a>
+            <a style="color: black" @click.prevent=""> Ok, Voltar :/ </a>
           </v-btn>
         </v-card-actions>
       </v-card>
     </v-col>
   </v-row>
 </template>
-
-<script>
-export default {}
-</script>
-
-<style scoped lang="scss"></style>
