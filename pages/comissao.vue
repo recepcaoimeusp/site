1095888b14449faaf2e607f09d0220d3f0a81bdf
Thiago Guerrero <tgbalera@hotmail.com>
2021-04-09 17:47:42 -0300

    Refactoring Quem Somos? page
    Signed-off-by: Thiago Guerrero <tgbalera@hotmail.com>
    Signed-off-by: Luiz Carlos <luizcarlosdk@hotmail.com>

--- pages/comissao.vue
+++ pages/comissao.vue
@@ -2,106 +2,118 @@
   <v-row>
     <v-col>
       <v-row class="mb-0 py-8 background-azul">
-        <v-col cols="12" class="d-flex justify-center align-center">
-          <div class="comissao">
-            <Logo class="mr-sm-5 mb-5 mb-sm-0" />
-            <h2 class="titulo">O que é a Comissão?</h2>
-            <span class="descricao-comissao justify-center">
-              <p>
-                A Comissão de Recepção é um grupo de veteranes do IME-USP
-                responsável por organizar a recepção e auxiliar os ingressantes
-                em seus primeiros momentos IMEanes. Apresenta cada detalhezinho
-                dessa maravilhosa universidade, desde como chegar lá, até os
-                truques de como sobreviver às disciplinas. Sabemos que esses
-                primeiros momentos no IME-USP podem ser um pouco difíceis, mas
-                estamos aqui para ajudar e fazer com que esse primeiro ano na
-                USP seja inesquecível!
-              </p>
-              <p>
-                Também organiza a super Semana de Recepção, repleta de
-                atividades pensadas e preparadas com muito carinho para integrar
-                os bixes a essa nova fase universitária. Incluímos atividades
-                desde palestras informativas (como a palestra do Diretor) até
-                atividades interativas para conhecer os colegas de turma,
-                afinal, é muito importante aproveitar essa jornada e fazer novas
-                amizades!
-              </p>
-              <p>
-                As atividades da Comissão de Recepção não se limitam apenas ao
-                planejamento da Semana de Recepção. Também organizamos o
-                Kit-Bixe (um kit completo para você mostrar por aí que passou no
-                IME-USP), o Guia do Bixe (repleto de informações que serão muito
-                úteis para a sua graduação inteira), além de outros eventos de
-                integração e plantões de dúvidas. Antes e depois da Semana de
-                Recepção, estamos sempre à disposição para tirar dúvidas e
-                auxiliar os ingressantes nesses primeiros momentos IMEanes!
-              </p>
-            </span>
-            <ul class="d-flex justify-space-between socialMedia">
-              <li class="nome text-center">
-                <v-icon color="black" size="55px">mdi-facebook</v-icon>
-                <a
-                  href="https://www.facebook.com/recepcaoimeusp"
-                  class="link"
-                  target="_blank"
-                  >recepcaoimeusp</a
-                >
-              </li>
-              <li class="nome text-center">
-                <v-icon color="black" size="55px">mdi-instagram</v-icon>
-                <a
-                  href="https://www.instagram.com/recepcaoimeusp/"
-                  target="_blank"
-                  class="link"
-                  >@recepcaoimeusp</a
-                >
-              </li>
-              <li class="nome text-center">
-                <v-icon color="black" size="55px">mdi-discord</v-icon>
-                <a
-                  href="https://discord.com/invite/P6Zn86nUve"
-                  class="link"
-                  target="_blank"
-                  >IME 2021</a
-                >
-              </li>
-            </ul>
-          </div>
+        <v-col cols="12" class="comissao">
+          <v-card flat style="background: none">
+            <v-card-title>
+              <Logo class="mx-auto" />
+            </v-card-title>
+            <v-card-text>
+              <h2 class="text--primary titulo">O que é a Comissão?</h2>
+              <span class="text--primary text-center my-auto">
+                <p>
+                  A Comissão de Recepção é um grupo de veteranes do IME-USP
+                  responsável por organizar a recepção e auxiliar os
+                  ingressantes em seus primeiros momentos IMEanes. Apresenta
+                  cada detalhezinho dessa maravilhosa universidade, desde como
+                  chegar lá, até os truques de como sobreviver às disciplinas.
+                  Sabemos que esses primeiros momentos no IME-USP podem ser um
+                  pouco difíceis, mas estamos aqui para ajudar e fazer com que
+                  esse primeiro ano na USP seja inesquecível!
+                </p>
+                <p>
+                  Também organiza a super Semana de Recepção, repleta de
+                  atividades pensadas e preparadas com muito carinho para
+                  integrar os bixes a essa nova fase universitária. Incluímos
+                  atividades desde palestras informativas (como a palestra do
+                  Diretor) até atividades interativas para conhecer os colegas
+                  de turma, afinal, é muito importante aproveitar essa jornada e
+                  fazer novas amizades!
+                </p>
+                <p>
+                  As atividades da Comissão de Recepção não se limitam apenas ao
+                  planejamento da Semana de Recepção. Também organizamos o
+                  Kit-Bixe (um kit completo para você mostrar por aí que passou
+                  no IME-USP), o Guia do Bixe (repleto de informações que serão
+                  muito úteis para a sua graduação inteira), além de outros
+                  eventos de integração e plantões de dúvidas. Antes e depois da
+                  Semana de Recepção, estamos sempre à disposição para tirar
+                  dúvidas e auxiliar os ingressantes nesses primeiros momentos
+                  IMEanes!
+                </p>
+              </span>
+            </v-card-text>
+          </v-card>
+          <v-card-actions>
+            <v-container>
+              <v-icon
+                class="mx-5"
+                tag="a"
+                href="https://www.facebook.com/recepcaoimeusp"
+                target="_blank"
+                color="black"
+                size="55px"
+                >mdi-facebook</v-icon
+              >
+
+              <v-icon
+                class="mx-5"
+                href="https://www.instagram.com/recepcaoimeusp/"
+                target="_blank"
+                color="black"
+                size="55px"
+                >mdi-instagram</v-icon
+              >
+              <v-icon
+                class="mx-5"
+                href="https://discord.com/invite/P6Zn86nUve"
+                target="_blank"
+                color="black"
+                size="55px"
+                >mdi-discord</v-icon
+              >
+            </v-container>
+          </v-card-actions>
         </v-col>
       </v-row>
       <v-row>
-        <div class="apresentacao">
+        <v-col align="center">
           <h2 class="titulo">Conheça a Comissão!</h2>
-        </div>
-        <v-row class="d-flex justify-center membros-comissao">
-          <ImagesCarrossel
-            :altura="tamanhoImagem()"
-            :largura="tamanhoImagem()"
-            image-path="comissao/azul"
-            image-name="comissao"
-            number-of-images="28"
-            extension="jpg"
-            :intervalo="12000"
-          />
-          <ImagesCarrossel
-            :altura="tamanhoImagem()"
-            :largura="tamanhoImagem()"
-            image-path="comissao/vermelho"
-            image-name="comissao"
-            number-of-images="9"
-            extension="jpg"
-            :intervalo="12000"
-          />
-          <ImagesCarrossel
-            :altura="tamanhoImagem()"
-            :largura="tamanhoImagem()"
-            image-path="comissao/amarelo"
-            image-name="comissao"
-            number-of-images="19"
-            extension="jpg"
-            :intervalo="12000"
-          />
-        </v-row>
+          <v-row justify="center">
+            <v-col cols="12" lg="4" md="6" sm="12">
+              <ImagesCarrossel
+                :altura="tamanhoImagem()"
+                :largura="tamanhoImagem()"
+                image-path="comissao/azul"
+                image-name="comissao"
+                number-of-images="28"
+                extension="jpg"
+                :intervalo="12000"
+              />
+            </v-col>
+            <v-col cols="12" lg="4" md="6" sm="12">
+              <ImagesCarrossel
+                :altura="tamanhoImagem()"
+                :largura="tamanhoImagem()"
+                image-path="comissao/vermelho"
+                image-name="comissao"
+                number-of-images="9"
+                extension="jpg"
+                :intervalo="12000"
+              />
+            </v-col>
+            <v-col cols="12" lg="4" md="6" sm="12">
+              <ImagesCarrossel
+                :altura="tamanhoImagem()"
+                :largura="tamanhoImagem()"
+                image-path="comissao/amarelo"
+                image-name="comissao"
+                number-of-images="19"
+                extension="jpg"
+                :intervalo="12000"
+              />
+            </v-col>
+          </v-row>
+        </v-col>
       </v-row>
     </v-col>
   </v-row>
@@ -136,41 +148,12 @@ export default {
 </script>
 
 <style scoped lang="scss">
-.socialMedia {
-  list-style-type: none;
-  width: 100%;
-  padding-top: 3em;
-  flex-wrap: wrap;
-  .nome {
-    margin-top: 10px;
-    vertical-align: center;
-  }
-  .icon {
-    vertical-align: middle;
-    margin-right: 5px;
-  }
-  .link {
-    color: #000;
-  }
-}
 .comissao {
   max-width: 900px;
-  justify-content: center;
   text-align: center;
-}
-.apresentacao {
-  width: 100%;
-  text-align: center;
-  flex-direction: column;
-  align-items: center;
+  margin: 0 auto;
 }
 .titulo {
   padding: 1em;
-}
-.membros-comissao > * {
-  margin: 2em;
-}
-.descricao-comissao {
-  max-width: 800px;
 }
 </style>
