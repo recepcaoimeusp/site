1ea706df167f63c4ec49e6dbaf91e32795c15a2b
luizcarlosdk1 <luizcarlosdk@usp.br>
2021-04-29 11:10:49 -0300

    Fix lint problems
    Signed-off-by: luizcarlosdk1 <luizcarlosdk@usp.br>

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -101,7 +101,8 @@ export default {
         ehExterno: true,
       },
       {
-        url: 'https://drive.google.com/drive/folders/1Rje607bFQQOlgvBoRSubSEjzc0EV1LMF?usp=sharing',
+        url:
+          'https://drive.google.com/drive/folders/1Rje607bFQQOlgvBoRSubSEjzc0EV1LMF?usp=sharing',
         nome: 'Vídeos',
         ehExterno: true,
       },
