d4856979ff2a0c8b85625bdb37e545fc1273a9ef
José Lucas Silva Mayer <josemayer@usp.br>
2023-02-23 05:34:20 -0400

    Atualiza seções da página principal com frame do Scooby
    Signed-off-by: José Lucas Silva Mayer <josemayer@usp.br>

--- pages/index.vue
+++ pages/index.vue
@@ -9,13 +9,24 @@
         background-colored
         min-height-row="400px"
         align-row="start"
+        align-col="start"
         border-top
       >
         <template #center>
+	  <v-container>
           <v-row class="px-5">
-            <v-col class="d-flex flex-column align-center" cols="12" md="12">
-              <h1 class="my-5 welcomeTitle txtBrown">Bem-vinde ao IME!</h1>
-              <p align="center" class="welcomeParagraph txtBrown">
+            <v-col>
+              <div class="scoobyArquimedes">
+                <Frame
+                  ><img
+                    width="300px"
+                    src="~@/assets/images/scooby-arquimedes.png"
+                /></Frame>
+              </div>
+            </v-col>
+            <v-col class="d-flex flex-column" cols="12" md="6">
+              <h1 class="my-5 welcomeTitle txtBlack">Bem-vinde ao IME!</h1>
+              <p class="welcomeParagraph txtGray">
                 Ser aprovado na USP não é uma tarefa fácil. E é uma honra, para
                 nós, recebê-lo(a) no nosso queridíssimo e incrível instituto.
                 Agora é hora de comemorar, porque, daqui em diante, você é
@@ -23,54 +34,13 @@
               </p>
             </v-col>
           </v-row>
-          <v-row class="generalWelcomeInfo">
-            <v-col
-              v-for="item in welcomeInfos"
-              :key="item"
-              class="pa-10"
-              cols="12"
-              md="6"
-            >
-              <h2 class="py-5 txtGreen">{{ item.title }}</h2>
-              <p>{{ item.desc }}</p>
-              <p>
-                <v-btn color="#7A9244" :to="item.buttonLink" outlined>{{
-                  item.buttonTitle
-                }}</v-btn>
-              </p>
-            </v-col>
-          </v-row>
-          <PageBar background-colored align-col="start">
-            <template #left>
-              <center>
-                <img class="py-8 shrekFace" src="/shrek.png" />
-              </center>
-            </template>
-            <template #right>
-              <div class="stripThemeInfo pa-8">
-                <h2 class="py-5 txtLightGreen">Faz o urro!</h2>
-                <p class="white--text">
-                  Esse ano, o tema da nossa tão esperada semana de recepção é o
-                  Shrek! Nós preparamos, com todo carinho, muitas atividades e
-                  itens que são a cara do nosso ogrinho favorito!
-                </p>
-                <p>
-                  <v-btn
-                    href="https://instagram.com/recepcaoimeusp"
-                    color="#E1DFB6"
-                    outlined
-                    >visite nosso instagram</v-btn
-                  >
-                </p>
-              </div>
-            </template>
-          </PageBar>
           <main class="mt-5">
-            <h1 align="center" class="timelineTitle py-5 txtBrown">
+            <h1 align="center" class="timelineTitle py-5 txtBlack">
               Passei! E agora?
             </h1>
             <EventosTimeline :eventos="eventos" />
           </main>
+	  </v-container>
         </template>
       </PageBar>
     </v-col>
@@ -81,31 +51,17 @@
 import Logo from '~/components/LogoComissao.vue'
 import EventosTimeline from '~/components/EventosTimeline.vue'
 import PageBar from '~/components/PageBar.vue'
+import Frame from '~/components/Frame.vue'
 
 export default {
   components: {
     Logo,
     EventosTimeline,
     PageBar,
+    Frame,
   },
   data: () => ({
     eventos: [],
-    welcomeInfos: [
-      {
-        title: 'A Comissão',
-        desc:
-          'A comissão de recepção do IME é formada por um grupo de veteranas e veteranos, cujo objetivo é trazer a melhor experiência para os ingressantes da universidade e mostrar o melhor que a USP tem a oferecer durante a semana de recepção.',
-        buttonTitle: 'Quem somos',
-        buttonLink: '/comissao',
-      },
-      {
-        title: 'O Instituto',
-        desc:
-          'O Instituto de Matemática e Estatística, ou IME, é uma das principais unidades da USP focadas nas ciências exatas. Ele é composto por quatro departamentos principais, os quais realizam pesquisas e oferecem diversas matérias aos cursos de graduação e pós-graduação do instituto.',
-        buttonTitle: 'Conheça o IME',
-        buttonLink: '/ime',
-      },
-    ],
   }),
   async beforeMount() {
     const sheetID = '1KNeMUcH96RjCT9km99wVrz-6Yl4BX7rSAJjxrz8aGEc'
@@ -157,7 +113,6 @@ export default {
 
 .welcomeParagraph {
   max-width: 700px;
-  padding: 10px;
   font-family: Chewy;
 }
 
@@ -177,7 +132,8 @@ export default {
   border-radius: 10px;
 }
 
-.shrekFace {
-  width: 220px;
+.scoobyArquimedes {
+  display: grid;
+  place-items: center;
 }
 </style>
