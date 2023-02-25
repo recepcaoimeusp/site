ec6029167b56e5eeb02575acc2eb04b0626f5142
José Lucas Silva Mayer <josemayer@usp.br>
2023-02-25 02:31:34 -0400

    Adiciona logo e foco nas paginações internas
    Esta atualização também modifica alguns estilos anteriormente aplicados,
    adequando-os à estilização global e às classes já prontas do Vuetify.
    
    Signed-off-by: José Lucas Silva Mayer <josemayer@usp.br>

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -3,12 +3,13 @@
     <v-app-bar
       fixed
       app
-      color="#A15AA0"
-      class="hidden-md-and-down mainNav bgBeige"
+      flat
+      height="85"
+      class="hidden-md-and-down mainNav bgViolet"
     >
       <v-toolbar-title>
-        <NuxtLink style="color: #ffffff" to="/">
-          Comissão de Recepção IME USP
+        <NuxtLink class="link-box px-3" to="/">
+          <Logo alternative white has-custom-height custom-height="65" />
         </NuxtLink>
       </v-toolbar-title>
       <v-spacer />
@@ -17,43 +18,42 @@
         <NuxtLink
           v-if="!link.ehExterno"
           :key="`link-${i}`"
-          style="color: #ffffff"
-          class="me-5"
+          class="link-box white--text px-5"
           :to="link.url"
-          >{{ link.nome }}</NuxtLink
         >
+          {{ link.nome }}
+        </NuxtLink>
         <a
           v-else
           :key="`link-${i}`"
           :href="link.url"
-          class="me-5"
-          style="color: #ffffff"
+          class="white--text px-5"
           target="_blank"
-          >{{ link.nome }}</a
         >
+          {{ link.nome }}
+        </a>
       </template>
     </v-app-bar>
 
-    <v-app-bar fixed dark class="bgBeige hidden-lg-and-up">
+    <v-app-bar fixed dark class="bgViolet hidden-lg-and-up">
       <v-app-bar-nav-icon
-        class="txtDarkGreen"
+        class="white--text"
         @click.stop="drawer = !drawer"
       ></v-app-bar-nav-icon>
     </v-app-bar>
 
     <v-navigation-drawer
       v-model="drawer"
-      class="mainNav bgBeige"
+      class="mainNav bgViolet"
       fixed
       temporary
     >
       <v-list nav dense>
-        <v-list-item-group active-class="text--accent-4 txtDarkGreen">
-          <v-btn class="bgGreen white--text my-5" :href="forms" target="_blank"
-            >Bixe, cadastre-se aqui</v-btn
-          >
+        <v-list-item-group active-class="text--accent-4">
           <v-list-item to="/">
-            <v-list-item-title>Página Inicial</v-list-item-title>
+            <v-list-item-title class="white--text">
+              Página Inicial
+            </v-list-item-title>
           </v-list-item>
           <template v-for="(link, i) in links">
             <v-list-item
@@ -62,10 +62,14 @@
               :href="link.url"
               target="_blank"
             >
-              <v-list-item-title>{{ link.nome }}</v-list-item-title>
+              <v-list-item-title class="white--text">
+                {{ link.nome }}
+              </v-list-item-title>
             </v-list-item>
             <v-list-item v-else :key="i" :to="link.url">
-              <v-list-item-title>{{ link.nome }}</v-list-item-title>
+              <v-list-item-title class="white--text">
+                {{ link.nome }}
+              </v-list-item-title>
             </v-list-item>
           </template>
         </v-list-item-group>
@@ -75,15 +79,23 @@
 </template>
 
 <script>
+import Logo from '~/components/LogoComissao.vue'
 export default {
   name: 'NavBar',
+  components: {
+    Logo,
+  },
   data: () => ({
     drawer: false,
     mostrarInput: false,
     forms: '',
     links: [
       { url: '/comissao', nome: 'Quem somos?', ehExterno: false },
-      { url: '/semana', nome: 'Semana de Recepção', ehExterno: false },
+      {
+        url: '/semana',
+        nome: 'Semana de Recepção',
+        ehExterno: false,
+      },
       {
         url: 'https://cheersshop.com.br/kit/3516',
         nome: 'Kit Bixe',
@@ -119,5 +131,27 @@ export default {
 <style>
 .mainNav {
   font-family: Chewy;
+}
+
+.link-box {
+  height: 85px;
+  display: flex;
+  align-items: center;
+}
+
+.nuxt-link-exact-active {
+  position: relative;
+  background-color: rgba(255, 255, 255, 0.1);
+}
+
+.nuxt-link-exact-active::after {
+  content: '';
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 3px;
+  background-color: #fff;
+  border-radius: 10px;
 }
 </style>
