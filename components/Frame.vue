25f1df2fcc2f2f17a2b85d9fda7904e69df32bd5
José Lucas Silva Mayer <josemayer@usp.br>
2023-02-25 22:04:59 -0400

    Adiciona componente de flores nas bordas do Frame
    Signed-off-by: José Lucas Silva Mayer <josemayer@usp.br>

--- components/Frame.vue
+++ components/Frame.vue
@@ -1,17 +1,35 @@
 <template>
-  <div class="frame" :class="{ 'blue-border' : blue }">
+  <div class="frame" :class="{ 'blue-border': blue }">
+    <div class="border-flowers-top">
+      <Flowers :number="flowersTop" />
+    </div>
     <div :class="blue ? 'bgBlue' : 'bgGreen'">
       <slot></slot>
+    </div>
+    <div class="border-flowers-bottom">
+      <Flowers :number="flowersBottom" />
     </div>
   </div>
 </template>
 
 <script>
+import Flowers from '~/components/Flowers.vue'
 export default {
+  components: {
+    Flowers,
+  },
   props: {
     blue: {
       type: Boolean,
       default: false,
+    },
+    flowersTop: {
+      type: Number,
+      default: 0,
+    },
+    flowersBottom: {
+      type: Number,
+      default: 0,
     },
   },
 }
@@ -21,8 +39,21 @@ export default {
 .frame {
   border: 70px solid transparent;
   border-image: url(~@/assets/images/bordas.svg) 60.47 round;
+  position: relative;
 }
 .blue-border {
   border-image: url(~@/assets/images/bordas-blue.svg) 60.47 round;
+}
+.border-flowers-top {
+  position: absolute;
+  top: -80px;
+  left: 0;
+  width: 100%;
+}
+.border-flowers-bottom {
+  position: absolute;
+  bottom: 30px;
+  left: 0;
+  width: 100%;
 }
 </style>
