c0dcdfca221b99861822cb25c9b8c15b1e0b0fce
José Lucas Silva Mayer <josemayer@usp.br>
2023-02-25 21:47:54 -0400

    Adiciona Frame em versão azul
    Signed-off-by: José Lucas Silva Mayer <josemayer@usp.br>

--- components/Frame.vue
+++ components/Frame.vue
@@ -1,16 +1,28 @@
 <template>
-  <div class="frame">
-    <div class="bgGreen">
+  <div class="frame" :class="{ 'blue-border' : blue }">
+    <div :class="blue ? 'bgBlue' : 'bgGreen'">
       <slot></slot>
     </div>
   </div>
 </template>
 
-<script></script>
+<script>
+export default {
+  props: {
+    blue: {
+      type: Boolean,
+      default: false,
+    },
+  },
+}
+</script>
 
 <style scoped>
 .frame {
   border: 70px solid transparent;
   border-image: url(~@/assets/images/bordas.svg) 60.47 round;
+}
+.blue-border {
+  border-image: url(~@/assets/images/bordas-blue.svg) 60.47 round;
 }
 </style>
