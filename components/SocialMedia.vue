349727bfa03a6fec2997b4f89b38c0836eb41cb3
FernandoGLima <fernandolimabusiness@gmail.com>
2024-01-29 23:41:31 -0300

    Created a new component of Social Media links and change color

---
+++ components/SocialMedia.vue
@@ -0,0 +1,50 @@
+<template>
+    <v-card-actions>
+          <v-container>
+            <v-icon
+              class="mx-5"
+              tag="a"
+              href="https://www.facebook.com/recepcaoimeusp"
+              target="_blank"
+              color="#fff"
+              size="35px"
+              >fa-brands fa-facebook</v-icon
+            >
+
+            <v-icon
+              class="mx-5"
+              tag="a"
+              href="https://www.instagram.com/imeusp.recepcao/"
+              target="_blank"
+              color="#fff"
+              size="35px"
+              >fa-brands fa-instagram</v-icon
+            >
+            <v-icon
+              class="mx-5"
+              tag="a"
+              href="https://discord.gg/qKXAkf8EjE"
+              target="_blank"
+              color="#fff"
+              size="35px"
+              >fa-brands fa-discord</v-icon
+            >
+            <v-icon
+              class="mx-5"
+              tag="a"
+              href="https://www.tiktok.com/@imeusp.recepcao"
+              target="_blank"
+              color="#fff"
+              size="35px"
+              >fa-brands fa-tiktok</v-icon
+              >
+          </v-container>
+        </v-card-actions>
+</template>
+
+
+<script>
+    export default{
+        name:'SocialMedia',
+    }
+</script>
