30edf034e353b53e1879f4b59cfa9f8d747f7505
José Lucas Silva Mayer <josemayer@usp.br>
2024-02-02 12:53:21 -0400

    Add texts to "Kit Bixe" and "Bottons" sections
    Signed-off-by: José Lucas Silva Mayer <josemayer@usp.br>

--- pages/kitbixe.vue
+++ pages/kitbixe.vue
@@ -8,25 +8,52 @@
           </div>
         </template>
         <template #right>
-          <div class="d-flex flex-column align-center align-md-start">
+          <div class="d-flex flex-column align-center align-md-start py-4">
             <h1 class="mb-4 mx-10 mx-md-0 txtBlack">Kit Bixe</h1>
-            <div class="mx-10 mx-md-0 txtBlack">
+            <div class="mx-10 mx-md-0 txtBlack desc">
               <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Quisquam voluptatum, quibusdam, quia, quae voluptates
-                voluptatibus quod voluptatem quos doloribus quas doloremque.
-                Quisquam voluptatum, quibusdam, quia, quae voluptates
-                voluptatibus quod voluptatem quos doloribus quas doloremque.
-                Quisquam voluptatum, quibusdam, quia, quae voluptates
-                voluptatibus quod voluptatem quos doloribus quas doloremque.
-                Quisquam voluptatum, quibusdam, quia, quae voluptates
+                Estar no primeiro semestre da faculdade é um momento repleto de
+                novas experiências e desafios. E para te ajudar a aproveitar ao
+                máximo essa fase, apresentamos o nosso incrível
+                <strong>Kit Bixe</strong>: Um conjunto elaborado com tudo o que
+                um estudante precisa para iniciar essa etapa. Nossos kits
+                oferecem itens-chave de materiais de estudo e outros produtos
+                gerais que são úteis para o dia a dia, tais como:
+              </p>
+              <ul class="mb-2">
+                <li>Caderno</li>
+                <li>Estojo</li>
+                <li>Camiseta</li>
+                <li>Caneca</li>
+                <li>Sacochila</li>
+                <li>Chaveiro</li>
+                <li>Cordão para cartão de ônibus</li>
+              </ul>
+              <p>
+                O Kit bixe pode ser encomendado previamente, utilizando o botão
+                abaixo, ou no estande presencial da Comissão durante a semana de
+                recepção, enquanto durarem os estoques! Caso você não tenha
+                interesse em adquirir todos os itens em conjunto, também é
+                possível adquiri-los separadamente ao longo da semana de
+                recepção.
+              </p>
+              <p>
+                Além disso, temos a iniciativa do Kit bixe acessível — uma forma
+                gratuita de adquirir o produto, destinado exclusivamente aos
+                ingressantes de baixa renda, que pode ser solicitado de acordo
+                com um comprovante de condição socioeconômica. Esses kits são
+                limitados e distribuídos de acordo com a ordem de solicitação.
               </p>
             </div>
             <div
               class="d-flex flex-column flex-sm-row align-center mx-10 buttons mx-md-0 mt-6"
             >
-              <Button>Encomende seu kit</Button>
-              <Button>Solicite seu kit acessível</Button>
+              <Button :href="formsKit" target="_blank"
+                >Encomende seu kit</Button
+              >
+              <Button :href="formsAffordableKit" target="_blank"
+                >Solicite seu kit acessível</Button
+              >
             </div>
           </div>
         </template>
@@ -38,22 +65,18 @@
           </div>
         </template>
         <template #right>
-          <div class="d-flex flex-column align-center align-md-start">
+          <div class="d-flex flex-column align-center align-md-start py-4">
             <h1 class="mb-4 mx-10 mx-md-0 txtBlack">Bottons</h1>
-            <div class="mx-10 mx-md-0 txtBlack">
+            <div class="mx-10 mx-md-0 txtBlack desc">
               <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Quisquam voluptatum, quibusdam, quia, quae voluptates
-                voluptatibus quod voluptatem quos doloribus quas doloremque.
-                Quisquam voluptatum, quibusdam, quia, quae voluptates
-                voluptatibus quod voluptatem quos doloribus quas doloremque.
-                Quisquam voluptatum, quibusdam, quia, quae voluptates
-                voluptatibus quod voluptatem quos doloribus quas doloremque.
-                Quisquam voluptatum, quibusdam, quia, quae voluptates
+                Agora que você é IMEane, é hora de mostrar para o mundo! Com
+                nossos bottons, você pode personalizar mochilas, jaquetas e
+                camisetas, além de colecioná-los para usar de acordo com o seu
+                humor do dia. São 8 modelos diferentes, cada um com uma estampa
+                exclusiva, que você pode adquirir separadamente ou em conjunto,
+                apenas durante a semana de recepção, no estande da Comissão. As
+                unidades são limitadas, então garanta o seu o quanto antes!
               </p>
-            </div>
-            <div class="mx-10 mx-md-0 mt-6">
-              <Button>Encomende seus bottons</Button>
             </div>
           </div>
         </template>
@@ -71,15 +94,19 @@ export default {
     PageBar,
     Button,
   },
+  data: () => ({
+    formsKit: 'https://forms.gle/cgSf7xELMdwsCWHW8',
+    formsAffordableKit: 'https://forms.gle/eJXSfox73TvWxH1o6',
+  }),
   computed: {
     kitSize() {
       switch (this.$vuetify.breakpoint.name) {
         case 'xs':
-          return 300
+          return 350
         case 'sm':
-          return 400
-        default:
           return 450
+        default:
+          return 500
       }
     },
     bottonsSize() {
@@ -101,7 +128,7 @@ h1 {
   font-family: Gang of Three;
 }
 
-p {
+.desc {
   font-family: Fuji;
   font-weight: 500;
 }
